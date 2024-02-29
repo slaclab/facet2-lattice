adad2ef2f401ca94c78500a44ab55e5bbbac1896
David B. Cesar <dcesar@sdfiana007.sdf.slac.stanford.edu>
2024-02-28 17:04:52 -0800

    Downloaded MAD Feb 2024 update from CVS. This update added two BPMs and an unpowered linac section that were inadvertantly removed from a previous release. Ran /Lucretia/scripts/FACET2e_Lucretia.m to generate a new FACET2e.mat and copied that to the models directory. Checked that beta functions and dispersion are essentially the same in the two models. There is a slight difference around 5m (backtracked to the injector) and at 920m (near the IP) where the beta functions get tight. It is unclear what causes the difference, but it does not seem to matter. I did check that the sextupoles, quads, and cavities all have the same positions and locations in the two models.

--- MAD/get_from_slac.bash
+++ MAD/get_from_slac.bash
@@ -1,4 +1,7 @@
-scp slac:~/CVS/optics/etc/lattice/facet2/* .
+# on an afs cpu pull from cvs, like: 
+#cvs -d /afs/slac/g/lcls/cvs checkout optics/etc/lattice/facet2/
+# then copy those files to here
+scp dcesar@cent7a:~/optics/etc/lattice/facet2/* .
 
 
 #scp slac:~/CVS/optics/etc/lattice/lcls/bmad/* .
